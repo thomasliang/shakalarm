db1dd8ed63f169ea913419688d4d205796731b13
Wing <poonlokwing@gmail.com>
2014-02-27 20:20:14 +0800

    Wing

--- src/com/example/shakalarm/MainActivity.java
+++ src/com/example/shakalarm/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 public class MainActivity extends Activity {
 //useless
 //fiona
+//Wing
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
