88fe8b51799425128860aa2093fe13ff196b1ee7
handsome <handsome@wf230-171.ust.hk>
2014-02-27 19:37:03 +0800

    hahaha

--- src/com/example/shakalarm/MainActivity.java
+++ src/com/example/shakalarm/MainActivity.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
-
+//useless
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
