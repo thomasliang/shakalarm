501c15ef8fe6e7cd3dc4edde233a18c808d5f894
Wing <poonlokwing@gmail.com>
2014-03-05 21:29:46 +0800

    Followed COMP3111H lab (again) to show you it works --Wing

--- src/com/example/shakalarm/MainActivity.java
+++ src/com/example/shakalarm/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.shakalarm;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements android.view.View.OnClickListener{
+	Button btnLeft, btnRight;
+	TextView textDisplay;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//initialize instances
+		btnLeft = (Button) findViewById(R.id.btnLeft);
+		btnRight = (Button) findViewById(R.id.btnRight);
+		textDisplay = (TextView) findViewById(R.id.textDisplay);
+		
+		//Set listener
+		btnLeft.setOnClickListener(this);
+		btnRight.setOnClickListener(this);
 	}
 
 	@Override
@@ -17,5 +34,15 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	@Override
+	public void onClick(View view) {
+		if (view == btnLeft) {
+			textDisplay.setText(R.string.left);
+		} else if (view == btnRight) {
+			textDisplay.setText(R.string.right);
+		}
+	}
+	
 
 }
