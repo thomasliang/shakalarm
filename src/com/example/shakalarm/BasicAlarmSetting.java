ae85919cbfc0ee499d674b25ff219561edf7aa7b
Wing <poonlokwing@gmail.com>
2014-03-14 21:56:07 +0800

    comments added

--- src/com/example/shakalarm/BasicAlarmSetting.java
+++ src/com/example/shakalarm/BasicAlarmSetting.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
+/**
+ * This activity class corresponds to basic_alarm_setting_layout,
+ * it is used for displaying the setting page
+ * @author Wing
+ * last modified: 3/14
+ */
 public class BasicAlarmSetting extends Activity {
 
 	@Override
@@ -18,7 +24,7 @@ public class BasicAlarmSetting extends Activity {
 		testButton.setOnClickListener(new OnClickListener() {
 
 			@Override
-			public void onClick(View v) {
+			public void onClick(View v) { //link to the button in the B
 			}
 		});
 
