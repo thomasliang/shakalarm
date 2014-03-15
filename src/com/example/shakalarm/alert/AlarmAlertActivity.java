5e1d1d5847e1b72a21b83f8672ae54206a13cd3a
Wing <poonlokwing@gmail.com>
2014-03-15 22:51:47 +0800

    thread, back after 5 seconds (failed)

--- src/com/example/shakalarm/alert/AlarmAlertActivity.java
+++ src/com/example/shakalarm/alert/AlarmAlertActivity.java
@@ -16,95 +16,81 @@ import com.example.shakalarm.R;
 public class AlarmAlertActivity extends Activity implements OnClickListener {
 
 	private Alarm alarm;
-	
+
 	//private MediaPlayer mediaPlayer;
 	//private Vibrator vibrator;
 
 	private boolean alarmActive;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		final Window window = getWindow();
-		window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-				| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-				| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+		//		final Window window = getWindow();
+		//		window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
+		//				| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+		//		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+		//				| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
 
 		//TODO alarm_alert layout 
 		setContentView(R.layout.alarm_alert);
 
-		Bundle bundle = this.getIntent().getExtras();
-		alarm = (Alarm) bundle.getSerializable("alarm");
-
-		this.setTitle(alarm.getAlarmName());
-
-		//Mode selection
-		switch (alarm.getMode()) {
-		case NORMAL:
-            //...
-			break;
-		case SHAKING:
-            //...
-			break;
-		case BLOWING:
-			//...
-			break;
-		}
-
-		startAlarm();
-
+		//		Bundle bundle = this.getIntent().getExtras();
+		//		alarm = (Alarm) bundle.getSerializable("alarm");
+		//
+		//		this.setTitle(alarm.getAlarmName());
+		//
+		//		//Mode selection
+		//		switch (alarm.getMode()) {
+		//		case NORMAL:
+		//            //...
+		//			break;
+		//		case SHAKING:
+		//            //...
+		//			break;
+		//		case BLOWING:
+		//			//...
+		//			break;
+		//		}
+		
+				startAlarm();
+				onDestroy();
 	}
-	
 
 	private void startAlarm() {
 
 		Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 		Ringtone r = RingtoneManager.getRingtone(getBaseContext(), notification);
 		r.play();
-		
+
 		//w1.release() here, please test it @Wing
-		AlarmManagerBroadcastReceiver.lockOff(getBaseContext());
+		//AlarmManagerBroadcastReceiver.lockOff(getBaseContext());
 
 	}
+
 	/*
-	 * for vibrating and ringTone playing use,
-	 * as those haven't finished,
-	 * not enabled now.
+	 * for vibrating and ringTone playing use, as those haven't finished, not
+	 * enabled now.
 	 * 
-	 * @author thomasleung
-	 * last modified 15/3/2014
+	 * @author thomasleung last modified 15/3/2014
 	 */
-	
-    /*
-	private void startAlarm() {
 
-		if (alarm.getAlarmTonePath() != "") {
-			mediaPlayer = new MediaPlayer();
-			if (alarm.getVibrate()) {
-				vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
-				long[] pattern = { 1000, 200, 200, 200 };
-				vibrator.vibrate(pattern, 0);
-			}
-			try {
-				mediaPlayer.setVolume(1.0f, 1.0f);
-				mediaPlayer.setDataSource(this,
-						Uri.parse(alarm.getAlarmTonePath()));
-				mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-				mediaPlayer.setLooping(true);
-				mediaPlayer.prepare();
-				mediaPlayer.start();
+	/*
+	 * private void startAlarm() {
+	 * 
+	 * if (alarm.getAlarmTonePath() != "") { mediaPlayer = new MediaPlayer(); if
+	 * (alarm.getVibrate()) { vibrator = (Vibrator)
+	 * getSystemService(VIBRATOR_SERVICE); long[] pattern = { 1000, 200, 200,
+	 * 200 }; vibrator.vibrate(pattern, 0); } try { mediaPlayer.setVolume(1.0f,
+	 * 1.0f); mediaPlayer.setDataSource(this,
+	 * Uri.parse(alarm.getAlarmTonePath()));
+	 * mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
+	 * mediaPlayer.setLooping(true); mediaPlayer.prepare(); mediaPlayer.start();
+	 * 
+	 * } catch (Exception e) { mediaPlayer.release(); alarmActive = false; } }
+	 * 
+	 * }
+	 */
 
-			} catch (Exception e) {
-				mediaPlayer.release();
-				alarmActive = false;
-			}
-		}
-
-	}
-	*/
-	
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -115,6 +101,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 		super.onResume();
 		alarmActive = true;
 	}
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -142,29 +129,38 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 	 * 
 	 * @see android.app.Activity#onDestroy()
 	 */
-	/*
+
 	@Override
 	protected void onDestroy() {
-		try {
-			if (vibrator != null)
-				vibrator.cancel();
-		} catch (Exception e) {
+		//		try {
+		//			if (vibrator != null)
+		//				vibrator.cancel();
+		//		} catch (Exception e) {
+		//
+		//		}
+		//		try {
+		//			mediaPlayer.stop();
+		//		} catch (Exception e) {
+		//
+		//		}
+		//		try {
+		//			mediaPlayer.release();
+		//		} catch (Exception e) {
+		//
+		//		}
+		Thread timer = new Thread() {
+			public void run() {
+				try {
+					sleep(5000);
+				} catch (InterruptedException e) {
 
-		}
-		try {
-			mediaPlayer.stop();
-		} catch (Exception e) {
-
-		}
-		try {
-			mediaPlayer.release();
-		} catch (Exception e) {
-
-		}
+				}
+			}
+		};
+		timer.start();
 		super.onDestroy();
 	}
-   */
-	
+
 	/*
 	 * Functions for detecting shaking & blowing
 	 */
@@ -173,19 +169,17 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 		//...
 		return correct;
 	}
-	
+
 	public boolean isBlown() {
 		boolean correct = false;
 		//...
 		return correct;
 	}
-	
+
 	@Override
 	public void onClick(View arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
-
-
