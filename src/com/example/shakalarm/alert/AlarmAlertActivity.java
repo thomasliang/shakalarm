b46d4f043d39cd0eb21e33dc12999806bc1fb175
thomasleung <thomasliang@me.com>
2014-03-16 00:07:23 +0800

    basic alarm 4
    fixing conflict

--- src/com/example/shakalarm/alert/AlarmAlertActivity.java
+++ src/com/example/shakalarm/alert/AlarmAlertActivity.java
@@ -1,6 +1,5 @@
 package com.example.shakalarm.alert;
 
-import com.example.shakalarm.alert.AlarmManagerBroadcastReceiver;
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -13,32 +12,20 @@ import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.Window;
-import android.view.WindowManager;
-<<<<<<< HEAD
 import android.widget.Button;
-=======
->>>>>>> FETCH_HEAD
-import android.widget.Toast;
 
-import com.example.shakalarm.Alarm;
 import com.example.shakalarm.R;
 
 @TargetApi(Build.VERSION_CODES.HONEYCOMB)
 @SuppressLint("NewApi")
 public class AlarmAlertActivity extends Activity implements OnClickListener {
 
-	private Alarm alarm;
-
-	//private MediaPlayer mediaPlayer;
-	//private Vibrator vibrator;
 
 	private boolean alarmActive;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-<<<<<<< HEAD
 
 		//TODO alarm_alert layout 
 		setContentView(R.layout.alarm_alert);	
@@ -66,38 +53,6 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 	{
 		this.finish();
 	}
-=======
-		//		final Window window = getWindow();
-		//		window.addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
-		//				| WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
-		//		window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
-		//				| WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-
-		//TODO alarm_alert layout 
-		setContentView(R.layout.alarm_alert);
-
-		//		Bundle bundle = this.getIntent().getExtras();
-		//		alarm = (Alarm) bundle.getSerializable("alarm");
-		//
-		//		this.setTitle(alarm.getAlarmName());
-		//
-		//		//Mode selection
-		//		switch (alarm.getMode()) {
-		//		case NORMAL:
-		//            //...
-		//			break;
-		//		case SHAKING:
-		//            //...
-		//			break;
-		//		case BLOWING:
-		//			//...
-		//			break;
-		//		}
-		
-				startAlarm();
-				this.finish();
-	}
->>>>>>> FETCH_HEAD
 
 	
 	   private void startAlarm() throws InterruptedException {
@@ -107,7 +62,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 		r.play();
 
 		//w1.release() here, please test it @Wing
-		//AlarmManagerBroadcastReceiver.lockOff(getBaseContext());
+		AlarmManagerBroadcastReceiver.lockOff(getBaseContext());
 
 	}
 
@@ -117,7 +72,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 	 * 
 	 * @author thomasleung last modified 15/3/2014
 	 */
-<<<<<<< HEAD
+
 	
   
 //       private void startAlarm() {
@@ -146,27 +101,9 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 //
 //	}
 
-	
-=======
 
-	/*
-	 * private void startAlarm() {
-	 * 
-	 * if (alarm.getAlarmTonePath() != "") { mediaPlayer = new MediaPlayer(); if
-	 * (alarm.getVibrate()) { vibrator = (Vibrator)
-	 * getSystemService(VIBRATOR_SERVICE); long[] pattern = { 1000, 200, 200,
-	 * 200 }; vibrator.vibrate(pattern, 0); } try { mediaPlayer.setVolume(1.0f,
-	 * 1.0f); mediaPlayer.setDataSource(this,
-	 * Uri.parse(alarm.getAlarmTonePath()));
-	 * mediaPlayer.setAudioStreamType(AudioManager.STREAM_ALARM);
-	 * mediaPlayer.setLooping(true); mediaPlayer.prepare(); mediaPlayer.start();
-	 * 
-	 * } catch (Exception e) { mediaPlayer.release(); alarmActive = false; } }
-	 * 
-	 * }
-	 */
 
->>>>>>> FETCH_HEAD
+
 	/*
 	 * (non-Javadoc)
 	 * 
@@ -205,7 +142,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 	 * 
 	 * @see android.app.Activity#onDestroy()
 	 */
-<<<<<<< HEAD
+
 
 //	@Override
 //	protected void onDestroy() {
@@ -229,7 +166,6 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 //	}
  
 	
-=======
 
 	@Override
 	protected void onDestroy() {
@@ -253,7 +189,7 @@ public class AlarmAlertActivity extends Activity implements OnClickListener {
 		super.onDestroy();
 	}
 
->>>>>>> FETCH_HEAD
+
 	/*
 	 * Functions for detecting shaking & blowing
 	 */
