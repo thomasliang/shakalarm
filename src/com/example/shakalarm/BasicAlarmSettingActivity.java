6a838c15381811dbd225f9043f7c9bc58f269874
handsome <handsome@wf175-193.ust.hk>
2014-03-19 19:06:26 +0800

    reference some code from existing project, still trying to figure out how they work.

--- src/com/example/shakalarm/BasicAlarmSettingActivity.java
+++ src/com/example/shakalarm/BasicAlarmSettingActivity.java
@@ -1,38 +1,129 @@
 package com.example.shakalarm;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.app.TimePickerDialog;
+import android.app.AlertDialog.Builder;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.content.ActivityNotFoundException;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.DialogInterface.OnClickListener;
+import android.content.DialogInterface.OnMultiChoiceClickListener;
+import android.content.res.Configuration;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
+import android.view.HapticFeedbackConstants;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.AnalogClock;
-import android.widget.Button;
+import android.view.Window;
+import android.view.View.OnTouchListener;
+import android.widget.CheckedTextView;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
-public class BasicAlarmSettingActivity extends Activity {
+public class BasicAlarmSettingActivity extends ListActivity {
+	ImageButton deleteButton;
+	TextView okButton;
+	TextView cancelButton;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// Get the AlarmManager Service
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alarm_settings);
+		//don't know yet what this do
+		//requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
-		//AnalogClock analogClock = (AnalogClock) findViewById(R.id.analog_clock);
-		
-		//press confirm button and go back to main activity
-		/*final Button confirm_button = 
-				(Button) findViewById(R.id.confirm_button);
-		confirm_button.setOnClickListener(new OnClickListener() {
+		deleteButton = (ImageButton) findViewById(R.id.toolbar).findViewById(R.id.button_delete);
+		deleteButton.setOnTouchListener(new OnTouchListener() {
 			@Override
-			public void onClick(View v) {
-				Intent intentMain = new Intent(BasicAlarmSettingActivity.this, MainActivity.class);
-				BasicAlarmSettingActivity.this.startActivity(intentMain);
-	            //Log.i("Content "," Main layout ");
+			public boolean onTouch(View v, MotionEvent event) {
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					deleteButton.setBackgroundColor(getResources().getColor(R.color.holo_blue_light));
+					break;
+				case MotionEvent.ACTION_UP:
+					v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+					Builder dialog = new AlertDialog.Builder(BasicAlarmSettingActivity.this);
+					dialog.setTitle("Delete");
+					dialog.setMessage("Delete this alarm?");
+					//function to be added
+					dialog.setPositiveButton("Ok", new OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+						}
+					});
+					dialog.setNegativeButton("Cancel", new OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							dialog.dismiss();
+						}
+					});
+					dialog.show();
+				//not yet sure what this do						
+				case MotionEvent.ACTION_MOVE:
+				case MotionEvent.ACTION_CANCEL:
+					deleteButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+					break;
+				}
+				return true;
 			}
-			
-		});*/
+		});
+		
+		okButton = (TextView) findViewById(R.id.textView_OK);
+		okButton.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					okButton.setBackgroundColor(getResources().getColor(R.color.holo_blue_light));
+					break;
+				case MotionEvent.ACTION_UP:
+					v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+				case MotionEvent.ACTION_MOVE:
+				case MotionEvent.ACTION_CANCEL:
+					okButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+					break;
+				}
+				return true;
+			}
+		});
+
+		cancelButton = (TextView) findViewById(R.id.textView_cancel);
+		cancelButton.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					cancelButton.setBackgroundColor(getResources().getColor(R.color.holo_blue_light));
+					break;
+				case MotionEvent.ACTION_UP:
+					v.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY);
+					finish();
+				case MotionEvent.ACTION_MOVE:
+				case MotionEvent.ACTION_CANCEL:
+					cancelButton.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+					break;
+				}
+				return true;
+			}
+		});
 	}
 
 	@Override
