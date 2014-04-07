e19543e4c9cecc3eb87e850590b2b03135014309
thomasleung <thomasliang@me.com>
2014-04-07 21:54:57 +0800

    Make the setting button as the test button
    /Music/Fox.mp3 should exist in order to set it as a ringTone

--- src/alarm/AlarmActivity.java
+++ src/alarm/AlarmActivity.java
@@ -1,5 +1,7 @@
 package alarm;
 
+import java.util.Calendar;
+
 import shakalarm.alarm.R;
 import stopwatch.StopWatchActivity;
 import timer.TimerActivity;
@@ -16,6 +18,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.HapticFeedbackConstants;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -139,6 +142,34 @@ public class AlarmActivity extends ListActivity implements android.view.View.OnC
 			}
 		});
 
+		final View settingButton = (ImageButton) findViewById(shakalarm.alarm.R.id.Setting_tab);
+		settingButton.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					settingButton.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
+					break;
+				case MotionEvent.ACTION_UP:
+					settingButton.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_dark));
+	                Alarm test = new Alarm();
+	                Calendar testTime = Calendar.getInstance();
+	                testTime.add(13,3);
+	                test.setAlarmTonePath(Environment.getExternalStorageDirectory()
+	        .getPath() + "/Music/Fox.mp3");
+	                test.setAlarmTime(testTime);
+	                test.schedule(getBaseContext());
+	                break;
+	                
+				case MotionEvent.ACTION_MOVE:
+				case MotionEvent.ACTION_CANCEL:
+					settingButton.setBackgroundColor(getResources().getColor(android.R.color.holo_orange_dark));
+					break;
+				}
+				return true;
+			}
+		});
 		//The alarm list in the center
 		alarmListView = (ListView) findViewById(android.R.id.list);
 		alarmListView.setLongClickable(true);
